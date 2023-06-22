3fd24de1698e4c104b7b713c70f57f2a8b71bb9d
khoalnd172103 <khoaly090141@gmail.com>
2023-06-22 20:37:07 +0700

    remove BookstoreApplicationTests class

--- src/test/java/com/khoalnd/bookstore/BookstoreApplicationTests.java
+++ src/test/java/com/khoalnd/bookstore/BookstoreApplicationTests.java
@@ -1,13 +1,13 @@
 package com.khoalnd.bookstore;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BookstoreApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class BookstoreApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
